87fe79b39560588d43d8e166c8a78387cb1b1c4a
GreatYYX <bigyyx@gmail.com>
2017-04-21 11:16:29 -0700

    update file iterator examples

--- examples/file_iter.py
+++ examples/file_iter.py
@@ -6,22 +6,22 @@ iter = tk.get_file_iterator('file_iter_test.txt', type='text')
 for id, value in iter:
     print id, value
 
-iter = tk.get_file_iterator('file_iter_test.jsonl', type='json_line', id_path='id_str', value_path='content')
+iter = tk.get_file_iterator('file_iter_test.jsonl', type='json_line', id_path='id_str')
 for id, value in iter:
     print id, value
 
-iter = tk.get_file_iterator('file_iter_test.csv',
-                            type='csv', id_column='id', value_columns=['content']) # field_names=['xx', 'yy']
+iter = tk.get_file_iterator('file_iter_test.csv', type='csv', id_column='id')
 for id, value in iter:
     print id, value
 
 print '----'
-iter1 = tk.get_file_iterator('file_iter_test.txt', type='text')
+# iter1 = tk.get_file_iterator('file_iter_test.txt', type='text')
+iter1 = tk.get_file_iterator('file_iter_test.csv', type='csv', id_column='id')
 for id, value in iter1:
     print id, value
     break
 iter2 = iter1.copy()
-for id, value in iter1:
+for id, value in iter1: # starts from id2
     print id, value
-for id, value in iter2:
+for id, value in iter2: # starts from id2
     print id, value
