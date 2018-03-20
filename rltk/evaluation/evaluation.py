4496a997d2218c477bc93b6d5418d7e94cc85ce0
greatyyx <bigyyx@gmail.com>
2018-03-20 15:30:43 -0700

    update apis for evaluation

--- rltk/evaluation/evaluation.py
+++ rltk/evaluation/evaluation.py
@@ -1,6 +1,29 @@
+from rltk.evaluation.trial import Trial
+
 class Evaluation():
-    def __init__(self, groud_truth=None):
+    def __init__(self):
         pass
 
     def add_trial(self):
+        pass
+
+    def false_positives(self, trial: Trial) -> float:
+        pass
+
+    def true_positives(self, trial: Trial) -> float:
+        pass
+
+    def false_negatives(self, trial: Trial) -> float:
+        pass
+
+    def true_negatives(self, trial: Trial) -> float:
+        pass
+
+    def precision(self, trial: Trial) -> float:
+        pass
+
+    def recall(self, trial: Trial) -> float:
+        pass
+
+    def f_measure(self, trial: Trial) -> float:
         pass
