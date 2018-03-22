918d8cd8af0796ae0fd38da8f6bc7b8aa9d866d0
greatyyx <bigyyx@gmail.com>
2018-03-21 17:50:03 -0700

    add file adapter

--- rltk/io/adapter/__init__.py
+++ rltk/io/adapter/__init__.py
@@ -1,2 +1,3 @@
 from rltk.io.adapter.adapter import Adapter
 from rltk.io.adapter.memory_adapter import MemoryAdapter
+from rltk.io.adapter.file_adapter import FileAdapter
---
+++ rltk/io/adapter/file_adapter.py
@@ -0,0 +1,30 @@
+import dbm
+import pickle
+
+from rltk.io.adapter import Adapter
+from rltk.record import Record
+
+
+class FileAdapter(Adapter):
+    def __init__(self, filename, dbm_class=dbm.ndbm):
+        """
+        :dbm_class dbm, dbm.gnu, dbm.ndbm, dbm.dumb (same as dbm)
+        """
+        self._db = dbm_class.open(filename, 'c')
+
+        # not all dbm supports iterator, simulate an iterator by getting a key set
+        self._ids = set([k for k in self._db.keys()])
+
+    def get(self, record_id):
+        return pickle.loads(self._db.get(record_id))
+
+    def set(self, record_id, record: Record):
+        self._ids.add(record_id)
+        self._db[record_id] = pickle.dumps(record)
+
+    def __next__(self):
+        for i in self._ids:
+            yield self.get(i)
+
+    def __del__(self):
+        self._db.close()
