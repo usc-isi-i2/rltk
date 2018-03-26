ba59dff33665d5d7cbdecf8614cb22caf2423e0f
greatyyx <bigyyx@gmail.com>
2018-03-26 10:56:36 -0700

    add array reader

--- rltk/io/reader/__init__.py
+++ rltk/io/reader/__init__.py
@@ -1,4 +1,5 @@
 from rltk.io.reader.reader import Reader
+from rltk.io.reader.array_reader import ArrayReader
 from rltk.io.reader.csv_reader import CSVReader
 from rltk.io.reader.jsonlines_reader import JsonLinesReader
 from rltk.io.reader.block_reader import BlockReader
--- rltk/io/reader/reader.py
+++ rltk/io/reader/reader.py
@@ -1,5 +1,6 @@
 import io
 
+
 class Reader(object):
     def __init__(self):
         pass
