5dc8a270399e4ce27a4dcb353b6285395a6b9746
greatyyx <bigyyx@gmail.com>
2018-03-27 11:34:05 -0700

    update custom block generator to support latest block writer

--- rltk/blocking/__init__.py
+++ rltk/blocking/__init__.py
@@ -1,1 +1,3 @@
 from rltk.blocking.block_generator import BlockGenerator
+from rltk.blocking.custom_block_generator import CustomBlockGenerator
+# from rltk.blocking.inverted_index_block_generator import InvertedIndexBlockGenerator
--- rltk/blocking/block_generator.py
+++ rltk/blocking/block_generator.py
@@ -3,16 +3,19 @@ from rltk.dataset import Dataset
 
 
 class BlockGenerator(object):
-    def __init__(self, dataset1 : Dataset, dataset2 : Dataset, writer: BlockWriter):
+    def __init__(self, dataset1 : Dataset, dataset2 : Dataset, writer: BlockWriter, **kwargs):
         self._writer = writer
         self._dataset1 = dataset1
         self._dataset2 = dataset2
+        self._kwargs = kwargs
 
-        self._generate()
+    def generate(self):
+
+        self._generate_blocks()
 
         handler = self._writer.get_handler()
         self._writer.close()
         return handler
 
-    def _generate(self):
+    def _generate_blocks(self):
         raise NotImplementedError
