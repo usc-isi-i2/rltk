7caf658abfae54ae9b6484e7dae2e1ebf1ed45f2
greatyyx <bigyyx@gmail.com>
2018-04-03 11:49:20 -0700

    add redis adapter

--- rltk/io/adapter/__init__.py
+++ rltk/io/adapter/__init__.py
@@ -1,3 +1,4 @@
 from rltk.io.adapter.key_value_adapter import KeyValueAdapter
 from rltk.io.adapter.memory_adapter import MemoryAdapter
 from rltk.io.adapter.dbm_adapter import DBMAdapter
+from rltk.io.adapter.redis_adapter import RedisAdapter
