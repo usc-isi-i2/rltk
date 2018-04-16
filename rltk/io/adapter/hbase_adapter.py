efe5c9f78833ab0dfd1400291623e88ffb397e6c
greatyyx <bigyyx@gmail.com>
2018-04-16 11:07:58 -0700

    fix timeout for hbase adapter

--- rltk/io/adapter/hbase_adapter.py
+++ rltk/io/adapter/hbase_adapter.py
@@ -6,10 +6,22 @@ from rltk.io.serializer import Serializer, PickleSerializer
 
 
 class HBaseAdapter(KeyValueAdapter):
+    """
+    Note: Need to increase timeout for thrift in hbase-site.xml
+    <property>
+        <name>hbase.thrift.server.socket.read.timeout</name>
+        <value>6000000</value>
+    </property>
+    <property>
+        <name>hbase.thrift.connection.max-idletime</name>
+        <value>18000000</value>
+    </property>
+    """
+
     def __init__(self, host, table, serializer: Serializer=None, key_prefix='', **kwargs):
         if not serializer:
             serializer = PickleSerializer()
-        self._conn = happybase.Connection(host=host, **kwargs)
+        self._conn = happybase.Connection(host=host, timeout=None, **kwargs)
         self._serializer = serializer
         self._key_prefix = key_prefix
         self._family_name = 'rltk'
@@ -33,7 +45,7 @@ class HBaseAdapter(KeyValueAdapter):
         self._conn.create_table(table_name, {self._family_name:dict()})
 
     def get(self, record_id) -> Record:
-        return self._serializer.loads(self._table.rows(self._get_key(record_id))[self._fam_col_name])
+        return self._serializer.loads(self._table.row(self._get_key(record_id))[self._fam_col_name])
 
     def set(self, record_id, record: Record):
         return self._table.put(self._get_key(record_id), {self._fam_col_name: self._serializer.dumps(record)})
@@ -42,5 +54,6 @@ class HBaseAdapter(KeyValueAdapter):
         return self.__next__()
 
     def __next__(self):
-        for key, data in self._table.scan(row_prefix=self._key_prefix.encode('utf-8')):
+        for key, data in self._table.scan(
+                row_prefix=self._key_prefix.encode('utf-8'), filter=b'FirstKeyOnlyFilter()'):
             yield self._serializer.loads(data[self._fam_col_name])
