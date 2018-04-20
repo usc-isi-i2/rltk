3a6ce8283f536dd36124e80351082df4ae11340d
greatyyx <bigyyx@gmail.com>
2018-04-20 16:13:01 -0700

    add requirements, include package data

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ from setuptools import find_packages
 with open('VERSION', 'r') as f:
     version = f.readline().strip()
 
-if not version or len(version) == 0:
-    exit()
+with open('requirements.txt', 'r') as f:
+    install_requires = list()
+    for line in f:
+        re = line.strip()
+        if re:
+            install_requires.append(re)
 
 setup(
     name='rltk',
@@ -15,5 +19,8 @@ setup(
     license='MIT',
     author='USC/ISI',
     author_email='',
-    description='Record Linkage ToolKit'
+    description='Record Linkage ToolKit',
+    package_data={'': ['LICENSE', 'VERSION', 'requirements.txt']},
+    include_package_data=True,
+    install_requires=install_requires
 )
