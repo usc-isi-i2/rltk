fd31f7e22316eeb12c3311d1738f3a2dc14e82fa
greatyyx <bigyyx@gmail.com>
2018-05-08 18:18:59 -0700

    add plot precision recall

--- examples/evaluation/evaluation.py
+++ examples/evaluation/evaluation.py
@@ -1,9 +1,8 @@
 import rltk
 
 
+@rltk.remove_raw_object
 class EvaluationRecord(rltk.Record):
-    remove_raw_object = True
-
     @rltk.cached_property
     def id(self):
         return self.raw_object['id']
@@ -13,9 +12,8 @@ class EvaluationRecord(rltk.Record):
         return self.raw_object['data']
 
 
+@rltk.remove_raw_object
 class EvaluationRecord2(rltk.Record):
-    remove_raw_object = True
-
     @rltk.cached_property
     def id(self):
         return self.raw_object['id']
@@ -42,12 +40,10 @@ ds2 = rltk.Dataset(reader=rltk.CSVReader(dataset_2_file_name),
 
 eva = rltk.Evaluation()
 
-
 for min_confidence_100 in range(0, 100):
     threshold = min_confidence_100 / 100
-
     trial = rltk.Trial(gt, min_confidence=0, top_k=0, save_record=True, key_1='data', key_2='data2',
-                       label="min threshold is: " + str(threshold), threshold=threshold)
+                       label='min threshold is: {}'.format(threshold), threshold=threshold)
     pairs = rltk.get_record_pairs(ds1, ds2)
     for r1, r2 in pairs:
         c = rltk.levenshtein_similarity(r1.data, r2.data2)
@@ -55,29 +51,26 @@ for min_confidence_100 in range(0, 100):
         trial.add_result(r1, r2, p, c)
 
     trial.evaluate()
-
-    print('false positives of ' + str(threshold) + '(threshold) is: ' + str(trial.false_positives))
     eva.add_trial(trial)
 
-coord = [
-    {
-        'x': 'threshold',
-        'y': 'false_positives',
-        'label': '123'
-    },
-
-    {
-        'x': 'threshold',
-        'y': 'true_positives',
-        'label': '456',
-        'linestyle': '--'
-    },
-
-    {
-        'x': 'recall',
-        'y': 'precision',
-        'label': 'pr',
-        'linestyle': '--'
-    }
-]
-eva.plot(coord)
+# coord = [
+#     {
+#         'x': 'threshold',
+#         'y': 'false_positives',
+#         'label': '123'
+#     },
+#     {
+#         'x': 'threshold',
+#         'y': 'true_positives',
+#         'label': '456',
+#         'linestyle': '--'
+#     },
+#     {
+#         'x': 'recall',
+#         'y': 'precision',
+#         'label': 'pr',
+#         'linestyle': '--'
+#     }
+# ]
+# eva.plot(coord)
+eva.plot_precision_recall()
--- rltk/evaluation/evaluation.py
+++ rltk/evaluation/evaluation.py
@@ -3,36 +3,42 @@ from rltk.evaluation.trial import Trial
 
 
 class Evaluation(object):
-    def __init__(self, trial_list: list = []):
+    def __init__(self, trial_list: list = None):
+        if not trial_list:
+            trial_list = []
         self.trial_list = trial_list
 
     def add_trial(self, trial: Trial):
-        if self.trial_list == None:
-            self.trial_list = []
         self.trial_list.append(trial)
 
     def plot(self, parameter_list):
 
-        if self.trial_list == None:
-            raise Exception("Empty Trial List, init it Firstly")
+        if len(self.trial_list) == 0:
+            raise Exception("Empty trial list")
 
         plt.figure()
-        for param in parameter_list:
 
+        for param in parameter_list:
             x, y = [], []
 
             x_key, y_key = param['x'], param['y']
-            for trial in self.trial_list:
-                x.append(getattr(trial, x_key))
-                y.append(getattr(trial, y_key))
-
             other_parameters = {}
             for k in param.keys():
                 if k in ('x', 'y'):
                     continue
                 other_parameters[k] = param[k]
 
+            for trial in self.trial_list:
+                x.append(getattr(trial, x_key))
+                y.append(getattr(trial, y_key))
+
             plt.plot(x, y, **other_parameters)
 
         plt.legend(loc='upper right')
         plt.show()
+
+    def plot_precision_recall(self):
+        return self.plot([{
+            'x': 'precision',
+            'y': 'recall'
+        }])
