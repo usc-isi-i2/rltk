5f91686a519853b487b0777bf928549014d5c82d
greatyyx <bigyyx@gmail.com>
2018-06-29 16:49:57 -0700

    update basic example to show how to get associated records

--- examples/basic/basic.py
+++ examples/basic/basic.py
@@ -2,14 +2,17 @@ import rltk
 
 
 class Record1(rltk.Record):
-    @rltk.cached_property
+    @property
     def id(self):
         return self.raw_object['doc_id']
 
-    @rltk.cached_property
+    @property
     def value(self):
         return self.raw_object['doc_value']
 
+    @property
+    def parent_id(self):
+        return '4' if self.id == '1' else None
 
 class Record2(rltk.Record):
     @rltk.cached_property
@@ -31,5 +34,8 @@ pairs = rltk.get_record_pairs(ds1, ds2)
 for r1, r2 in pairs:
     print('-------------')
     print(r1.id, r1.value, '\t', r2.id, r2.value)
+    if r1.parent_id:
+        print('r1\'s parent', r1.parent_id, ds1.get_record(r1.parent_id).value)
     print('levenshtein_distance:', rltk.levenshtein_distance(r1.value, r2.value))
     print('levenshtein_similarity:', rltk.levenshtein_similarity(r1.value, r2.value))
+
