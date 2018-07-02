0250f59e74ab4b58175be30cd6b691c2f2750477
greatyyx <bigyyx@gmail.com>
2018-07-02 14:16:00 -0700

    get_record_pairs should be in utils

--- rltk/__init__.py
+++ rltk/__init__.py
@@ -5,27 +5,3 @@ from rltk.similarity import *
 from rltk.blocking import *
 from rltk.tokenizer import *
 from rltk.evaluation import *
-
-
-def get_record_pairs(dataset1: Dataset, dataset2: Dataset,
-                     block_reader: BlockReader = None,
-                     ground_truth: GroundTruth = None):
-    """
-    Generate pairs to compare.
-
-    Args:
-        dataset1 (Dataset): dataset 1
-        dataset2 (Dataset): dataset 2
-        block_reader (BlockReader, optional): block reader
-        ground_truth (GroundTruth, optional): ground truth
-    """
-    if block_reader:
-        for id1, id2 in block_reader:
-            yield dataset1.get_record(id1), dataset2.get_record(id2)
-    elif ground_truth:
-        for id1, id2, label in ground_truth:
-            yield dataset1.get_record(id1), dataset2.get_record(id2)
-    else:
-        for r1 in dataset1:
-            for r2 in dataset2:
-                yield r1, r2
--- rltk/utils.py
+++ rltk/utils.py
@@ -1,5 +1,11 @@
 import unicodedata
 
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from rltk.dataset import Dataset
+    from rltk.io.reader.block_reader import BlockReader
+    from rltk.evaluation.ground_truth import GroundTruth
+
 
 def check_for_none(*args):
     for arg in args:
@@ -21,3 +27,28 @@ def convert_list_to_set(s):
     if isinstance(s, list):
         s = set(s)
     return s
+
+
+def get_record_pairs(dataset1: 'Dataset',
+                     dataset2: 'Dataset',
+                     block_reader: 'BlockReader' = None,
+                     ground_truth: 'GroundTruth' = None):
+    """
+    Generate pairs to compare.
+
+    Args:
+        dataset1 (Dataset): dataset 1
+        dataset2 (Dataset): dataset 2
+        block_reader (BlockReader, optional): block reader
+        ground_truth (GroundTruth, optional): ground truth
+    """
+    if block_reader:
+        for id1, id2 in block_reader:
+            yield dataset1.get_record(id1), dataset2.get_record(id2)
+    elif ground_truth:
+        for id1, id2, label in ground_truth:
+            yield dataset1.get_record(id1), dataset2.get_record(id2)
+    else:
+        for r1 in dataset1:
+            for r2 in dataset2:
+                yield r1, r2
