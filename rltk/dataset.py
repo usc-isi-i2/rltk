84417275f0724908943d1227919e1be8d129d13e
greatyyx <bigyyx@gmail.com>
2018-07-13 11:41:53 -0700

    add sampling callback function while creating dataset

--- rltk/dataset.py
+++ rltk/dataset.py
@@ -14,12 +14,14 @@ class Dataset(object):
         record_class (type(Record), optional): Sub class of Record.
         adapter (KeyValueAdapter, optional): Specify where to store indexed data. Defaults to MemoryAdapter.
         size (int, optional): Maximum size of records will be in Dataset, defaults to None, can be used when debugging.
+        sampling_function (callable, optional): Sampling function, `raw_object` is the only parameter. \
+                                                If it returns True, record instance will be created.
     Note:
         Set reader, record_class and adapter if new a Dataset needs to be generated.
         If Dataset is already generated and stored in a permanent adapter, only adapter needs to be provided.
     """
     def __init__(self, reader: Reader = None, record_class: type(Record) = None, adapter: KeyValueAdapter = None,
-                 size: int = None):
+                 size: int = None, sampling_function: callable = None):
         if not adapter:
             adapter = MemoryAdapter()
         self._adapter = adapter
@@ -29,6 +31,7 @@ class Dataset(object):
         if reader and record_class:
             self._reader = reader
             self._record_class = record_class
+            self._sampling_function = sampling_function
 
             self._build_index()
 
@@ -37,13 +40,14 @@ class Dataset(object):
             raise ValueError('Reader or Record class is not provided.')
         size = 0
         for raw_object in self._reader:
-            record_instance = self._record_class(raw_object)
-            generate_record_property_cache(record_instance)
-            self._adapter.set(record_instance.id, record_instance)
+            if not self._sampling_function or self._sampling_function(raw_object):
+                record_instance = self._record_class(raw_object)
+                generate_record_property_cache(record_instance)
+                self._adapter.set(record_instance.id, record_instance)
 
-            size += 1
-            if self._size and size >= self._size:
-                break
+                size += 1
+                if self._size and size >= self._size:
+                    break
 
     def get_record(self, record_id):
         """
