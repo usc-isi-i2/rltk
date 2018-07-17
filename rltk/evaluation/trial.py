6141ecdf6b02bd9283c0b095321bd2a13d1566cd
greatyyx <bigyyx@gmail.com>
2018-07-17 14:22:07 -0700

    add prefix for nested objects

--- rltk/evaluation/trial.py
+++ rltk/evaluation/trial.py
@@ -15,12 +15,6 @@ class Trial(object):
 
     If save_record is true, it will save the record of all data.
     It is useful to check whether the similarity function is good, but will cost memory to store the data.
-
-    Attributes:
-        groud_truth (GroundTruth): whether to save the record data.
-        min_confidence (float): if the result has lower confidence than min confidence, it will not be saved. min_confidence = 0 means all result will be saved.
-        top_k (int): the max number of result to be saved. top_k = 0 means all result will be saved.
-        save_record (bool): whether to save the record data.
     """
 
     class Result:
@@ -62,181 +56,9 @@ class Trial(object):
 
         def get_property_names(self):
             return ['is_positive', 'confidence'] + list(self.extra_key_values.keys())
-
-    class Evaluator:
-        """
-        Evaluation compares the result of the trial and the ground truth (stored in trial),
-        count and store the number of true positive, true negative, false positive, false negative.
-
-        It can then provide the statistics of:
-            true positive, true negative, false positive, false negative, f_measure, precision, false_discovery
-
-        If save_record is true, it will save the record of all data in the provided trial besides the number of result
-
-        Attributes:
-            save_record (boolean): whether to save the record data
-            tp (int): number of true positive
-            tn (int): number of true negative
-            fp (int): number of false positive
-            fn (int): number of false negative
-            tp_list (list, optional): saved true positive list
-            tn_list (list, optional): saved true negative list
-            fp_list (list, optional): saved false positive list
-            fn_list (list, optional): saved false negative list
-        """
-
-        def __init__(self, save_record=False):
-            """
-            init the number of true positive, true negative, false positive, false negative to 0
-            init save_record, default is False
-            if save_record is True, init the true positive list, true negative list,
-            false positive list, false negative list to empty list
-
-            Args:
-                save_record (boolean): Whether save the record of data. Defaults to False.
-            """
-            self.save_record = save_record
-            self._reset()
-
-        def _reset(self):
-
-            self.tp = 0
-            self.tn = 0
-            self.fp = 0
-            self.fn = 0
-
-            self.tp_list = []
-            self.tn_list = []
-            self.fp_list = []
-            self.fn_list = []
-
-        def evaluate(self, ground_truth: GroundTruth, data: list):
-            self._reset()
-
-            for trial_result in data:
-                gt_positive = ground_truth.is_positive(trial_result.record1.id, trial_result.record2.id)
-                trial_positive = trial_result.is_positive
-
-                if trial_positive and gt_positive:
-                    self.tp += 1
-                    if self.save_record:
-                        self.tp_list.append(trial_result)
-                elif not trial_positive and not gt_positive:
-                    self.tn += 1
-                    if self.save_record:
-                        self.tn_list.append(trial_result)
-                elif trial_positive and not gt_positive:
-                    self.fp += 1
-                    if self.save_record:
-                        self.fp_list.append(trial_result)
-                elif not trial_positive and gt_positive:
-                    self.fn += 1
-                    if self.save_record:
-                        self.fn_list.append(trial_result)
-
-        def precision(self) -> float:
-            """
-            Based on the mathematical formula:
-                precision = true positive / (true positive + false positive)
-            Calculate and return the precision
-
-            Returns:
-                precision (float)
-            """
-            if self.tp + self.fp == 0:
-                return 0.0
-            return self.tp / (self.tp + self.fp)
-
-        def recall(self) -> float:
-            """
-            return the true positive ratio
-
-            Returns:
-                recall (float)
-            """
-            return self.true_positives()
-
-        def f_measure(self) -> float:
-            """
-            Based on the mathematical formula:
-                f_measure = 2 * true positive / (2 * true positive + false positive + false negative)
-            Calculate and return the f_measure
-
-            Returns:
-                f_measure (float)
-            """
-            base = 2 * self.tp + self.fp + self.fn
-            if base == 0:
-                return 0.0
-            return 2 * self.tp / base
-
-        def false_positives(self) -> float:
-            """
-            Based on the mathematical formula:
-                false positive ratio = false positive / (false positive + true negative)
-            Calculate and return the false positive ratio
-
-            Returns:
-                false positive ratio (float)
-            """
-            if (self.fp + self.tn) == 0:
-                return 0.0
-            return self.fp / (self.fp + self.tn)
-
-        def true_positives(self) -> float:
-            """
-            Based on the mathematical formula:
-                true positive ratio = true positive / (true positive + false negative)
-            Calculate and return the true positive ratio
-
-            Returns:
-                true positive ratio (float)
-            """
-            if (self.tp + self.fn) == 0:
-                return 0.0
-            return self.tp / (self.tp + self.fn)
-
-        def false_negatives(self) -> float:
-            """
-            Based on the mathematical formula:
-                false negative ratio = false negative / (false negative + true positive)
-            Calculate and return the false negative ratio
-
-            Returns:
-                false negative ratio (float)
-            """
-            if (self.tp + self.fn) == 0:
-                return 0.0
-            return self.fn / (self.tp + self.fn)
-
-        def true_negatives(self) -> float:
-            """
-            Based on the mathematical formula:
-                true negative ratio = true negative / (true negative + false positive)
-            Calculate and return the true negative ratio
-
-            Returns:
-                true negative ratio (float)
-            """
-            if (self.fp + self.tn) == 0:
-                return 0.0
-            return self.tn / (self.fp + self.tn)
-
-        def false_discovery(self):
-            """
-            Based on the mathematical formula:
-                false discovery = false positive / (false positive + true positive)
-            Calculate and return the false false discovery
-
-            Returns:
-                false discovery (float)
-            """
-            if (self.fp + self.tp) == 0:
-                return 0.0
-            return self.fp / (self.fp + self.tp)
             
     def __init__(self, ground_truth: GroundTruth, label: str = '', min_confidence: float = 0,
-                 top_k: int = 0, save_record: bool = False):
+                 top_k: int = 0, **kwargs):
         """
         init data.
 
@@ -244,7 +66,6 @@ class Trial(object):
             ground_truth (GroundTruth): whether to save the record data.
             min_confidence (float): if the result has lower confidence than min confidence, it will not be saved.
             top_k (int): the max number of result to be saved.
-            save_record (bool): whether to save the record data.
             key_1 (String): the attribute in first record be compared.
             key_2 (String): the attribute in second record be compared.
         """
@@ -252,10 +73,48 @@ class Trial(object):
         self._min_confidence = min_confidence
         self.label = label
         self._top_k = top_k
-
         self._results = []
-        self.save_record = save_record
-        self.evaluator = None
+        self.extra_key_values = kwargs
+
+    def add_property(self, key, value):
+        self.extra_key_values[key] = value
+
+    def __getattr__(self, key):
+        try:
+            return self.extra_key_values[key]
+        except:
+            raise AttributeError
+
+    def pre_evaluate(self):
+        self.tp = 0
+        self.tn = 0
+        self.fp = 0
+        self.fn = 0
+        self.tp_list = []
+        self.tn_list = []
+        self.fp_list = []
+        self.fn_list = []
+
+    def evaluate(self):
+        self.pre_evaluate()
+
+        for trial_result in self._results:
+            gt_positive = self._ground_truth.is_positive(trial_result.record1.id, trial_result.record2.id)
+            trial_positive = trial_result.is_positive
+
+            if trial_positive and gt_positive:
+                self.tp_list.append(trial_result)
+            elif not trial_positive and not gt_positive:
+                self.tn_list.append(trial_result)
+            elif trial_positive and not gt_positive:
+                self.fp_list.append(trial_result)
+            elif not trial_positive and gt_positive:
+                self.fn_list.append(trial_result)
+
+        self.tp = len(self.tp_list)
+        self.tn = len(self.tn_list)
+        self.fp = len(self.fp_list)
+        self.fn = len(self.fn_list)
 
     def add_result(self, record1: Record, record2: Record, is_positive: bool, confidence: float = 1, **kwargs) -> None:
         """
@@ -306,10 +165,6 @@ class Trial(object):
         """
         return self._ground_truth
 
-    def evaluate(self):
-        self.evaluator = self.Evaluator(self.save_record)
-        self.evaluator.evaluate(self._ground_truth, self._results)
-
     @property
     def precision(self) -> float:
         """
@@ -320,8 +175,9 @@ class Trial(object):
         Returns:
             precision (float)
         """
-        self.check_evaluator_init()
-        return self.evaluator.precision()
+        if (self.tp + self.fp) == 0:
+            return 0.0
+        return self.tp / (self.tp + self.fp)
 
     @property
     def recall(self) -> float:
@@ -331,8 +187,9 @@ class Trial(object):
         Returns:
             recall (float)
         """
-        self.check_evaluator_init()
-        return self.evaluator.recall()
+        if (self.tp + self.fn) == 0:
+            return 0.0
+        return self.tp / (self.tp + self.fn)
 
     @property
     def f_measure(self) -> float:
@@ -344,8 +201,7 @@ class Trial(object):
         Returns:
             f_measure (float)
         """
-        self.check_evaluator_init()
-        return self.evaluator.f_measure()
+        return 1 / 2 * (self.precision + self.recall)
 
     @property
     def false_positives(self) -> float:
@@ -357,8 +213,9 @@ class Trial(object):
         Returns:
             false positive ratio (float)
         """
-        self.check_evaluator_init()
-        return self.evaluator.false_positives()
+        if (self.fp + self.tn) == 0:
+            return 0.0
+        return self.fp / (self.fp + self.tn)
 
     @property
     def true_positives(self) -> float:
@@ -370,8 +227,9 @@ class Trial(object):
         Returns:
             true positive ratio (float)
         """
-        self.check_evaluator_init()
-        return self.evaluator.true_positives()
+        if (self.tp + self.fn) == 0:
+            return 0.0
+        return self.tp / (self.tp + self.fn)
 
     @property
     def false_negatives(self) -> float:
@@ -383,8 +241,9 @@ class Trial(object):
         Returns:
             false negative ratio (float)
         """
-        self.check_evaluator_init()
-        return self.evaluator.false_negatives()
+        if (self.tp + self.fn) == 0:
+            return 0.0
+        return self.fn / (self.tp + self.fn)
 
     @property
     def true_negatives(self) -> float:
@@ -396,8 +255,9 @@ class Trial(object):
         Returns:
             true negative ratio (float)
         """
-        self.check_evaluator_init()
-        return self.evaluator.true_negatives()
+        if (self.fp + self.tn) == 0:
+            return 0.0
+        return self.tn / (self.fp + self.tn)
 
     @property
     def false_discovery(self):
@@ -409,35 +269,27 @@ class Trial(object):
         Returns:
             false discovery (float)
         """
-        self.check_evaluator_init()
-        return self.evaluator.false_discovery()
-
-    def check_evaluator_init(self):
-        if not self.evaluator:
-            raise Exception("Please run evaluator first")
+        if (self.fp + self.tp) == 0:
+            return 0.0
+        return self.fp / (self.fp + self.tp)
 
     @property
     def true_positives_list(self):
-        self.check_evaluator_init()
-        return self.evaluator.tp_list
+        return self.tp_list
 
     @property
     def true_negatives_list(self):
-        self.check_evaluator_init()
-        return self.evaluator.tn_list
+        return self.tn_list
 
     @property
     def false_positives_list(self):
-        self.check_evaluator_init()
-        return self.evaluator.fp_list
+        return self.fp_list
 
     @property
     def false_negatives_list(self):
-        self.check_evaluator_init()
-        return self.evaluator.fn_list
+        return self.fn_list
 
-    @staticmethod
-    def generate_dataframe(results, record1_columns=None, record2_columns=None, result_columns=None, **kwargs):
+    def generate_dataframe(self, results, record1_columns=None, record2_columns=None, result_columns=None, **kwargs):
 
         table = []
         r1_columns = record1_columns
@@ -459,15 +311,19 @@ class Trial(object):
             r1_data = []
             r2_data = []
             res_data = []
+            gt_data = []
             for prop_name in r1_columns:
                 r1_data.append(getattr(result.record1, prop_name))
             for prop_name in r2_columns:
                 r2_data.append(getattr(result.record2, prop_name))
             for prop_name in res_columns:
                 res_data.append(getattr(result, prop_name))
+            gt_data.append(self._ground_truth.get_label(result.record1.id, result.record2.id))
 
             # append data
-            table.append([r1_data, r2_data] + res_data)
+            table.append(r1_data + r2_data + gt_data + res_data)
 
-        columns = [r1_columns, r2_columns] + res_columns
+        r1_columns = ['record1.{}'.format(p) for p in r1_columns]
+        r2_columns = ['record2.{}'.format(p) for p in r2_columns]
+        columns = r1_columns + r2_columns + ['ground_truth.label'] + res_columns
         return pd.DataFrame(table, columns=columns, **kwargs)
