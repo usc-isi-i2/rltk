6242772df0f2be4ed0da6ff0d489b52abc3d6e61
greatyyx <bigyyx@gmail.com>
2018-09-04 16:55:01 -0700

    support vector as input

--- rltk/blocking/canopy_block_generator.py
+++ rltk/blocking/canopy_block_generator.py
@@ -8,6 +8,18 @@ from rltk.blocking import BlockGenerator
 
 
 class CanopyBlockGenerator(BlockGenerator):
+    """
+    Canopy based block generator.
+
+    Args:
+        t1 (float): The loose distance.
+        t2 (float): The tight distance.
+        vectorize_function (Callable): Convert values in record to a vector. 
+                                    The signature is `vectorize(r: Record) -> List`.
+        distance_metric (Callable): Compute the distance between two vectors.
+                                    The signature is `distance(v1: List, v2: List) -> float`
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._t1 = self._kwargs.get('t1')
@@ -17,28 +29,29 @@ class CanopyBlockGenerator(BlockGenerator):
             raise ValueError('t1 should be greater than t2')
         if self._t1 <= 0 or self._t2 <= 0:
             raise ValueError('t1 and t2 should greater than 0')
+        self._distance_metric = self._kwargs.get('distance_metric')
 
     def _generate_blocks(self):
         vec_id_mapping = {}  # map data to
-        dataset = np.array([])
+        dataset = []
         for r1 in self._dataset1:
-            v = float(self._vectorize_function(r1))
-            k = self.encode_key(v)
-            vec_id_mapping[k] =vec_id_mapping.get(k, [])
+            v = self._vectorize_function(r1)
+            k = self._encode_key(v)
+            vec_id_mapping[k] = vec_id_mapping.get(k, [])
             vec_id_mapping[k].append((0, r1.id))  # 0 means dataset1
-            dataset = np.append(dataset, v)
+            dataset.append(v)
         for r2 in self._dataset2:
-            v = float(self._vectorize_function(r2))
-            k = self.encode_key(v)
+            v = self._vectorize_function(r2)
+            k = self._encode_key(v)
             vec_id_mapping[k] = vec_id_mapping.get(k, [])
             vec_id_mapping[k].append((1, r2.id))  # 1 means dataset2
-            dataset = np.append(dataset, v)
+            dataset.append(v)
 
-        clusters = self.run_canopy_clustering(dataset, self._t1, self._t2, self.euclidean_distance)
+        clusters = self._run_canopy_clustering(dataset, self._t1, self._t2, self._distance_metric)
         for c in clusters:
             id1s, id2s = [], []
             for vec in c:
-                ids = vec_id_mapping[self.encode_key(vec)]
+                ids = vec_id_mapping[self._encode_key(vec)]
                 for ds_id, r_id in ids:
                     if ds_id == 0:
                         id1s.append(r_id)
@@ -48,11 +61,11 @@ class CanopyBlockGenerator(BlockGenerator):
                 self._writer.write(id1, id2)
 
     @staticmethod
-    def encode_key(v):
+    def _encode_key(v):
         return json.dumps(v)
 
     @staticmethod
-    def run_canopy_clustering(dataset, t1, t2, distance_metric):
+    def _run_canopy_clustering(dataset, t1, t2, distance_metric):
         """
         The algorithm proceeds as follows, using two thresholds t1 (the loose distance) and t2 (the tight distance), 
         where t1 > t2.
@@ -71,7 +84,7 @@ class CanopyBlockGenerator(BlockGenerator):
             center_vec = dataset[center_idx]
             new_canopy = []
             delete_list = []
-            dataset = np.delete(dataset, center_idx, 0)
+            del dataset[center_idx]
 
             for d_idx in range(len(dataset)):
                 d = dataset[d_idx]
@@ -79,12 +92,10 @@ class CanopyBlockGenerator(BlockGenerator):
                 if distance < t1:
                     new_canopy.append(d)
                 if distance < t2:
-                    delete_list.append(d)
-            dataset = np.delete(dataset, delete_list, 0)
+                    delete_list.append(d_idx)
+
+            for d_idx in sorted(delete_list, reverse=True):
+                del dataset[d_idx]
             new_canopy.append(center_vec)  # add center
             canopies.append(new_canopy)
         return canopies
-
-    @staticmethod
-    def euclidean_distance(vec1, vec2):
-        return math.sqrt(((vec1 - vec2) ** 2).sum())
