7e0fc1babbb4e1e1b8d97950234a95e2adadbadf
greatyyx <bigyyx@gmail.com>
2018-09-05 13:52:15 -0700

    update docstring for serializers

--- rltk/io/serializer/pickle_serializer.py
+++ rltk/io/serializer/pickle_serializer.py
@@ -4,6 +4,9 @@ from rltk.io.serializer import Serializer
 
 
 class PickleSerializer(Serializer):
+    """
+    `Pickle serializer <https://docs.python.org/3/library/pickle.html>`_ .
+    """
 
     def loads(self, string):
         return pickle.loads(string)
--- rltk/io/serializer/serializer.py
+++ rltk/io/serializer/serializer.py
@@ -1,6 +1,28 @@
 class Serializer(object):
+    """
+    Serialize and deserialize object. This is the super class.
+    """
+
     def loads(self, obj):
+        """
+        Load a serialized object.
+         
+        Args:
+            obj (obj): For most of times, it's byte or string.
+            
+        Returns:
+            obj: Python object.
+        """
         raise NotImplementedError
 
     def dumps(self, obj):
+        """
+        Serialize the given object.
+        
+        Args:
+            obj (obj): Python object.
+            
+        Returns:
+            obj: Serialized object.
+        """
         raise NotImplementedError
