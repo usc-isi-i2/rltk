46093e137d6f9db34c0207f9d9a1de44468821b3
greatyyx <bigyyx@gmail.com>
2018-09-24 11:29:48 -0700

    expose add_records, update docstring of dataset

--- rltk/dataset.py
+++ rltk/dataset.py
@@ -12,43 +12,47 @@ class Dataset(object):
     
     Args:
         reader (Reader, optional): Input reader.
-        record_class (type(Record), optional): Sub class of Record.
-        adapter (KeyValueAdapter, optional): Specify where to store indexed data. Defaults to MemoryAdapter.
-        size (int, optional): Maximum size of records will be in Dataset, defaults to None, can be used when debugging.
+        record_class (type(Record), optional): Concrete class of :meth:`Record`.
+        adapter (KeyValueAdapter, optional): Specify where to store indexed data. Defaults to :meth:`MemoryAdapter`.
+        size (int, optional): Maximum size of records will be in :meth:`Dataset`, \
+                                                defaults to None, can be used when debugging.
         sampling_function (callable, optional): Sampling function, `raw_object` is the only parameter. \
                                                 If it returns True, record instance will be created.
     Note:
-        Set reader, record_class and adapter if new a Dataset needs to be generated.
-        If Dataset is already generated and stored in a permanent adapter, only adapter needs to be provided.
+        - Set `reader`, `record_class` and `adapter` if new a :meth:`Dataset` needs to be generated.
+        - If :meth:`Dataset` is already generated and stored in a permanent adapter, only adapter needs to be provided.
+        - `reader` can also be added later, but `record_class` needs to be provided.
         
     """
     def __init__(self, reader: Reader = None, record_class: type(Record) = None, adapter: KeyValueAdapter = None,
                  size: int = None, sampling_function: Callable = None):
-        if not adapter:
-            adapter = MemoryAdapter()
-        self._adapter = adapter
-        self._size = size
+        self._adapter = adapter or MemoryAdapter()
+        self._record_class = record_class
+        self._sampling_function = sampling_function
 
-        # build index
-        if reader and record_class:
-            self._reader = reader
-            self._record_class = record_class
-            self._sampling_function = sampling_function
+        if reader and self._record_class:
+            self.add_records(reader, size)
 
-            self._build_index()
-
-    def _build_index(self):
-        if not self._reader or not self._record_class:
-            raise ValueError('Reader or Record class is not provided.')
-        size = 0
-        for raw_object in self._reader:
+    def add_records(self, reader: Reader, size: int = None):
+        """
+        Add `records` to :meth:`Dataset` from `reader` .
+        
+        Args:
+            reader (Reader, optional): Input reader.
+            size (int, optional): Maximum size of records will be in :meth:`Dataset`, \
+                            defaults to None, can be used when debugging.
+        """
+        if not self._record_class:
+            raise ValueError('Record class is not provided.')
+        curr_size = 0
+        for raw_object in reader:
             if not self._sampling_function or self._sampling_function(raw_object):
                 record_instance = self._record_class(raw_object)
                 generate_record_property_cache(record_instance)
                 self._adapter.set(record_instance.id, record_instance)
 
-                size += 1
-                if self._size and size >= self._size:
+                curr_size += 1
+                if size and curr_size >= size:
                     break
 
     def get_record(self, record_id):
