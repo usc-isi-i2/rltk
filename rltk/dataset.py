277cc1030517959e69b7a49c74130ca0f57a4580
greatyyx <bigyyx@gmail.com>
2018-09-25 11:43:28 -0700

    support parallel processing while generating dataset

--- rltk/dataset.py
+++ rltk/dataset.py
@@ -1,6 +1,7 @@
 from rltk.io.reader import Reader
 from rltk.io.adapter import KeyValueAdapter, MemoryAdapter
 from rltk.record import Record, generate_record_property_cache, get_property_names
+from rltk.parallel_processor import ParallelProcessor
 
 import pandas as pd
 from typing import Callable
@@ -14,8 +15,9 @@ class Dataset(object):
         reader (Reader, optional): Input reader.
         record_class (type(Record), optional): Concrete class of :meth:`Record`.
         adapter (KeyValueAdapter, optional): Specify where to store indexed data. Defaults to :meth:`MemoryAdapter`.
-        size (int, optional): Maximum size of records will be in :meth:`Dataset`, \
-                                                defaults to None, can be used when debugging.
+        size (int, optional): Same as `size` in :meth:`add_records` .
+        pp_num_of_processor (int, optional): Same as `pp_num_of_processor` in :meth:`add_records` .
+        pp_max_size_per_input_queue (int, optional): Same as `pp_max_size_per_input_queue` in :meth:`add_records` .
         sampling_function (callable, optional): Sampling function, `raw_object` is the only parameter. \
                                                 If it returns True, record instance will be created.
     Note:
@@ -24,16 +26,19 @@ class Dataset(object):
         - `reader` can also be added later, but `record_class` needs to be provided.
         
     """
+
     def __init__(self, reader: Reader = None, record_class: type(Record) = None, adapter: KeyValueAdapter = None,
-                 size: int = None, sampling_function: Callable = None):
+                 size: int = None, sampling_function: Callable = None,
+                 pp_num_of_processor: int = 0, pp_max_size_per_input_queue: int = 200):
         self._adapter = adapter or MemoryAdapter()
         self._record_class = record_class
         self._sampling_function = sampling_function
 
         if reader and self._record_class:
-            self.add_records(reader, size)
+            self.add_records(reader, size, pp_num_of_processor, pp_max_size_per_input_queue)
 
-    def add_records(self, reader: Reader, size: int = None):
+    def add_records(self, reader: Reader, size: int = None,
+                    pp_num_of_processor: int = 0, pp_max_size_per_input_queue: int = 200):
         """
         Add `records` to :meth:`Dataset` from `reader` .
         
@@ -41,19 +46,44 @@ class Dataset(object):
             reader (Reader, optional): Input reader.
             size (int, optional): Maximum size of records will be in :meth:`Dataset`, \
                             defaults to None, can be used when debugging.
+            pp_num_of_processor (int, optional): Same as `num_of_processor` in :meth:`ParallelProcessor`. \ 
+                            If non-zero is given and adapter is `parallel_safe`, \
+                            this method will run in parallel mode. Defaults to 0.
+            pp_max_size_per_input_queue (int, optional): Same as `max_size_per_input_queue` in \
+                                    :meth:`ParallelProcessor`. Defaults to 200.
         """
-        if not self._record_class:
-            raise ValueError('Record class is not provided.')
-        curr_size = 0
-        for raw_object in reader:
-            if not self._sampling_function or self._sampling_function(raw_object):
-                record_instance = self._record_class(raw_object)
+
+        def generate(_raw_object):
+            if not self._sampling_function or self._sampling_function(_raw_object):
+                record_instance = self._record_class(_raw_object)
                 generate_record_property_cache(record_instance)
                 self._adapter.set(record_instance.id, record_instance)
 
-                curr_size += 1
-                if size and curr_size >= size:
-                    break
+        if not self._record_class:
+            raise ValueError('Record class is not provided.')
+        curr_size = 0
+
+        # serial
+        if pp_num_of_processor == 0 or not self._adapter.parallel_safe:
+            for raw_object in reader:
+                if not self._sampling_function or self._sampling_function(raw_object):
+                    generate(raw_object)
+                    curr_size += 1
+                    if size and curr_size >= size:
+                        break
+        # parallel
+        else:
+            pp = ParallelProcessor(input_handler=generate, num_of_processor=pp_num_of_processor,
+                                   max_size_per_input_queue=pp_max_size_per_input_queue)
+            pp.start()
+            for raw_object in reader:
+                if not self._sampling_function or self._sampling_function(raw_object):
+                    pp.compute(raw_object)
+                    curr_size += 1
+                    if size and curr_size >= size:
+                        break
+            pp.task_done()
+            pp.join()
 
     def set_record(self, record: Record):
         """
