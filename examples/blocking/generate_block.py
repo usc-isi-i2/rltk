40a0af3f5efde5c151aca22ce4bf9d66eea29ffa
greatyyx <bigyyx@gmail.com>
2018-10-11 15:07:41 -0700

    update example

--- examples/blocking/generate_block.py
+++ examples/blocking/generate_block.py
@@ -29,16 +29,18 @@ class Record2(rltk.Record):
         return self.raw_object['name'].split(' ')[1]
 
 
-def blocking_function(r1, r2):
-    return r1.first_name[:1] == r2.first_name[:1]
+def blocking_function(r):
+    return r.first_name[:1]
 
 
 ds1 = rltk.Dataset(reader=rltk.CSVReader('ds1.csv', delimiter=','), record_class=Record1)
 ds2 = rltk.Dataset(reader=rltk.JsonLinesReader('ds2.jl'), record_class=Record2)
 
-block_handler = rltk.CustomBlockGenerator(
-    ds1, ds2, custom_function=blocking_function,
+block_handler = rltk.HashBlockGenerator(
+    ds1, ds2, hash_function=blocking_function,
     writer=rltk.BlockArrayWriter()).generate()
+
+
 pairs = rltk.get_record_pairs(ds1, ds2, rltk.BlockArrayReader(block_handler))
 for r1, r2 in pairs:
     print(r1.id, r1.first_name, '\t', r2.id, r2.first_name)
