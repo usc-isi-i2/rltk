7a35b4d2e47ce550521ac577234ded4987b672bc
greatyyx <bigyyx@gmail.com>
2018-10-11 14:25:08 -0700

    update iterator

--- rltk/io/adapter/dbm_adapter.py
+++ rltk/io/adapter/dbm_adapter.py
@@ -24,20 +24,16 @@ class DBMAdapter(KeyValueAdapter):
         self._db = dbm_class.open(filename, 'c')
         self._serializer = serializer
 
-        # not all dbm supports iterator, simulate an iterator by getting a key set
-        # key will be in byte form once read out from dbm, needs to be converted to string
-        self._ids = set([k.decode('utf-8') for k in self._db.keys()])
-
     def get(self, record_id):
         return self._serializer.loads(self._db.get(record_id))
 
     def set(self, record_id, record: Record):
-        self._ids.add(record_id)
         self._db[record_id] = self._serializer.dumps(record)
 
     def __next__(self):
-        for i in self._ids:
-            yield self.get(i)
+        for k in self._db.keys():
+            id_ = k.decode('utf-8')
+            yield self.get(id_)
 
     def close(self):
         self._db.close()
