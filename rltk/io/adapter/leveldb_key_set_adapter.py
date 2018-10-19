501be3535148eb34833652b740d7a62b9ddc3cff
greatyyx <bigyyx@gmail.com>
2018-10-19 15:39:40 -0700

    add leveldb keyset adapter, update io testcases

---
+++ rltk/io/adapter/leveldb_key_set_adapter.py
@@ -0,0 +1,67 @@
+import shutil
+import os
+import plyvel
+
+from rltk.io.serializer import Serializer, PickleSerializer
+from rltk.io.adapter.key_set_adapter import KeySetAdapter
+
+
+class LevelDbKeySetAdapter(KeySetAdapter):
+
+    def __init__(self, path, name, serializer: Serializer=None):
+        if not serializer:
+            serializer = PickleSerializer()
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        os.mkdir(path)
+        self._db = plyvel.DB(path, create_if_missing=True)
+        self._prefix = '{name}_'.format(name=name)
+        self._prefix_db = self._db.prefixed_db(self._encode(self._prefix))
+        self._serializer = serializer
+
+    @staticmethod
+    def _encode(string):
+        return string.encode(encoding='utf-8')
+
+    @staticmethod
+    def _decode(bytes_):
+        return bytes_.decode(encoding='utf-8')
+
+    def _get(self, key):
+        v = self._prefix_db.get(key)
+        if not v:
+            return
+        return self._serializer.loads(v)
+
+    def get(self, key):
+        return self._get(self._encode(key))
+
+    def set(self, key, value):
+        if not isinstance(value, set):
+            raise ValueError('value must be a set')
+        self.delete(key)
+        self._prefix_db.put(self._encode(key), self._serializer.dumps(value))
+
+    def add(self, key, value):
+        set_ = self.get(key)
+        if not set_:
+            set_ = set([])
+        set_.add(value)
+        return self.set(key, set_)
+
+    def remove(self, key, value):
+        set_ = self.get(key)
+        if not set_:
+            return
+        set_.remove(value)
+        return self.set(key, set_)
+
+    def delete(self, key):
+        return self._prefix_db.delete(self._encode(key))
+
+    def __next__(self):
+        for key in self._prefix_db.iterator(include_value=False):
+            yield self._decode(key), self._get(key)
+
+    def close(self):
+        self._db.close()
--- rltk/tests/test_io_adapter.py
+++ rltk/tests/test_io_adapter.py
@@ -1,6 +1,8 @@
 import os
 import pytest
 import redis
+import tempfile
+import shutil
 
 from rltk.record import Record
 from rltk.io.adapter import *
@@ -19,8 +21,7 @@ class ConcreteRecord(Record):
 record = ConcreteRecord(raw_object={'id': 'id1', 'value': 'value1'})
 
 
-def test_memory_adapter():
-    adapter = MemoryDatasetAdapter()
+def _test_dataset_adapter(adapter):
     adapter.set(record.id, record)
     assert adapter.get(record.id).id == record.id
     assert adapter.get(record.id).value == record.value
@@ -29,28 +30,57 @@ def test_memory_adapter():
         break
 
 
-def test_dbm_adapter():
+def test_memory_dataset_adapter():
+    adapter = MemoryDatasetAdapter()
+    _test_dataset_adapter(adapter)
+
+
+def test_dbm_dataset_adapter():
     name = 'test_dbm_adapter'
     adapter = DbmDatasetAdapter(name)
-    adapter.set(record.id, record)
-    assert adapter.get(record.id).id == record.id
-    assert adapter.get(record.id).value == record.value
-    for r in adapter:
-        assert r.id == record.id
-        break
+    _test_dataset_adapter(adapter)
     if os.path.exists(name + '.db'):
         os.remove(name + '.db')
 
 
-def test_redis_adapter():
+def test_redis_dataset_adapter():
     try:
         adapter = RedisDatasetAdapter('127.0.0.1', key_format='test_{record_id}')
-        adapter.set(record.id, record)
-        assert adapter.get(record.id).id == record.id
-        assert adapter.get(record.id).value == record.value
-        for r in adapter:
-            assert r.id == record.id
-            break
+        _test_dataset_adapter(adapter)
+    except redis.exceptions.ConnectionError:
+        return
 
+
+def _test_key_set_adapter(adapter):
+    adapter.set('a', set(['1', '2', '3']))
+    assert adapter.get('a') == set(['1', '2', '3'])
+    adapter.add('a', '4')
+    assert adapter.get('a') == set(['1', '2', '3', '4'])
+    adapter.remove('a', '4')
+    assert adapter.get('a') == set(['1', '2', '3'])
+    for k, v in adapter:
+        assert k == 'a'
+        assert v == set(['1', '2', '3'])
+        break
+    adapter.delete('a')
+    assert adapter.get('a') is None
+
+
+def test_memory_key_set_adapter():
+    adapter = MemoryKeySetAdapter()
+    _test_key_set_adapter(adapter)
+
+
+def test_leveldb_key_set_adapter():
+    path = os.path.join(tempfile.tempdir, 'rltk_test_leveldb_key_set_adapter')
+    adapter = LevelDbKeySetAdapter(path, name='test')
+    _test_key_set_adapter(adapter)
+    shutil.rmtree(path)
+
+
+def test_redis_key_set_adapter():
+    try:
+        adapter = RedisDatasetAdapter('127.0.0.1', key_format='rltk_test_redis_key_set_adapter_{record_id}')
+        _test_dataset_adapter(adapter)
     except redis.exceptions.ConnectionError:
         return
