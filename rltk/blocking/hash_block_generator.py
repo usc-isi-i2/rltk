8089d04f89397fd2c32208f92cab112eec6dcd68
greatyyx <bigyyx@gmail.com>
2018-10-23 14:36:00 -0700

    make block and generate non static

--- rltk/blocking/hash_block_generator.py
+++ rltk/blocking/hash_block_generator.py
@@ -13,13 +13,12 @@ class HashBlockGenerator(BlockGenerator):
     Hash block generator.
     """
 
-    @staticmethod
-    def block(dataset, function_: Callable = None, property_: str = None,
+    def block(self, dataset, function_: Callable = None, property_: str = None,
               ks_adapter: KeySetAdapter = None):
         """
         The return of `property_` or `function_` should be string.
         """
-        ks_adapter = BlockGenerator._block_args_check(function_, property_, ks_adapter)
+        ks_adapter = super()._block_args_check(function_, property_, ks_adapter)
         for r in dataset:
             value = function_(r) if function_ else getattr(r, property_)
             if not isinstance(value, str):
@@ -27,9 +26,8 @@ class HashBlockGenerator(BlockGenerator):
             ks_adapter.add(value, r.id)
         return ks_adapter
 
-    @staticmethod
-    def generate(ks_adapter1: KeySetAdapter, ks_adapter2: KeySetAdapter, block_writer: BlockWriter = None):
-        block_writer = BlockGenerator._generate_args_check(block_writer)
+    def generate(self, ks_adapter1: KeySetAdapter, ks_adapter2: KeySetAdapter, block_writer: BlockWriter = None):
+        block_writer = super()._generate_args_check(block_writer)
         for block_id, id1s in ks_adapter1:
             for id1 in id1s:
                 block_writer.write(block_id, BlockDatasetID.Dataset1, id1)
