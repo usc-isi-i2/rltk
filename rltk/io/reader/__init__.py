310b647d2de719e8e3393c9fe8257d202ae7623d
greatyyx <bigyyx@gmail.com>
2018-11-09 17:10:33 -0800

    discard block reader & writer

--- rltk/io/reader/__init__.py
+++ rltk/io/reader/__init__.py
@@ -2,5 +2,4 @@ from rltk.io.reader.reader import Reader
 from rltk.io.reader.array_reader import ArrayReader
 from rltk.io.reader.csv_reader import CSVReader
 from rltk.io.reader.jsonlines_reader import JsonLinesReader
-from rltk.io.reader.block_reader import BlockReader
 from rltk.io.reader.ground_truth_reader import GroundTruthReader
--- rltk/io/writer/__init__.py
+++ rltk/io/writer/__init__.py
@@ -1,3 +1,2 @@
 from rltk.io.writer.writer import Writer
-from rltk.io.writer.block_writer import BlockWriter
 from rltk.io.writer.ground_truth_writer import GroundTruthWriter
