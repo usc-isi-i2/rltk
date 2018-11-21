727114cca5cc0161c9d8670ea3d9152c242e9ecf
greatyyx <bigyyx@gmail.com>
2018-11-21 14:01:12 -0800

    update cli prompt

--- rltk/__main__.py
+++ rltk/__main__.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print('No command\n')
         help_info()
+        sys.exit()
 
     cmd = sys.argv[1]
     sub_cmd = sys.argv[2:] if len(sys.argv) >= 3 else []
@@ -22,7 +23,9 @@ if __name__ == '__main__':
 
     if cmd in ('help', '--help', 'h', '-h'):
         help_info()
+        sys.exit()
 
+    sys.argv[0] = cmd  # replace prog name
     temp_path = os.path.join(tempfile.gettempdir(), 'rltk', 'remote')
     if not os.path.exists(temp_path):
         os.makedirs(temp_path, exist_ok=True)
