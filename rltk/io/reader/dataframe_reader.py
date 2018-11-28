85f12555eda16cb8af810acd4648a887bdf8f02b
Lituta <lituta@outlook.com>
2018-11-27 16:37:16 -0800

    update docs

--- rltk/io/reader/dataframe_reader.py
+++ rltk/io/reader/dataframe_reader.py
@@ -9,8 +9,8 @@ class DataFrameReader(Reader):
     
     Args:
         df (pandas.DataFrame): DataFrame.
-        keep_dataframe_default_index (bool, optional): if True, add a key "dataframe_default_index" holding the original
-                                                       index in df. [default value: False]
+        keep_dataframe_default_index (bool, optional): if True, add a key "dataframe_default_index" holding the \
+                                                       original index in df. [default value: False]
 
     """
 
