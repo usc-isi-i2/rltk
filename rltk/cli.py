ad3f723ef83a84cf5d8b69cf941e739e33dcee31
greatyyx <bigyyx@gmail.com>
2019-02-15 11:03:59 -0800

    add progressbar

--- rltk/cli.py
+++ rltk/cli.py
@@ -3,9 +3,10 @@ import sys
 SEPARATOR_LEN = 20
 
 
-def prompt(text: str, new_line: bool = True):
+def prompt(text: str, *args, new_line: bool = True, **kwargs):
     line_end = '\n' if new_line else ''
-    print(text, file=sys.stderr, end=line_end)
+    print(text, *args, file=sys.stdout, end=line_end, **kwargs)
+    sys.stdout.flush()
 
 
 def select(text: str, cases: list, default: int = None, case_sensitive: bool = False):
@@ -46,4 +47,33 @@ def confirm(text: str, default: bool = None):
     return select(text, cases=[('(Y)es', 'y',), ('(N)o', 'n')], default=default, case_sensitive=False) == 'y'
 
 
+class Progress(object):
 
+    def __init__(self, format_: str = '{}', start: str = 'Starting...', end: str = 'Done!'):
+        self._format = format_
+        self._prev_len = 0
+        self._start = start
+        self._end = end
+
+    def update(self, *args):
+        text = self._format.format(*args)
+
+        # clean up
+        prompt('\r' + ' ' * self._prev_len, new_line=False)
+
+        # overwrite
+        prompt('\r' + text, new_line=False)
+        self._prev_len = len(text)
+
+    def __enter__(self):
+        if self._start:
+            prompt(self._start, new_line=False)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._end:
+            prompt('\r' + self._end, new_line=False)
+
+
+def progress(*args, **kwargs):
+    return Progress(*args, **kwargs)
