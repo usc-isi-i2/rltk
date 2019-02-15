2eb48481cf45fa6002cd2ecb57cecb7afc7c8ae3
greatyyx <bigyyx@gmail.com>
2019-02-15 10:18:17 -0800

    add prompt and confirm

--- rltk/__init__.py
+++ rltk/__init__.py
@@ -9,4 +9,5 @@ from rltk.evaluation import *
 from rltk.utils import get_record_pairs
 from rltk.parallel_processor import ParallelProcessor
 from rltk.map_reduce import MapReduce
+import rltk.cli
 import rltk.remote
---
+++ rltk/cli.py
@@ -0,0 +1,49 @@
+import sys
+
+SEPARATOR_LEN = 20
+
+
+def prompt(text: str, new_line: bool = True):
+    line_end = '\n' if new_line else ''
+    print(text, file=sys.stderr, end=line_end)
+
+
+def select(text: str, cases: list, default: int = None, case_sensitive: bool = False):
+    """
+    cases = [('(Y)es, 'y'), ('(N)o', 'n')]
+    """
+    prompt('=' * SEPARATOR_LEN)
+    prompt(text)
+    case_text = []
+    for idx, c in enumerate(cases):
+        if default is not None and idx == default:
+            case_text.append('[{}]'.format(c[0]))
+        else:
+            case_text.append('{}'.format(c[0]))
+    prompt(' / '.join(case_text) + '\n')
+    valid_cases = [c[1] for c in cases]
+    if default is not None:
+        valid_cases.append('')
+    if not case_sensitive:
+        valid_cases = list(map(lambda x: x.lower(), valid_cases))
+
+    while True:
+        user_input = input().strip()
+        if not case_sensitive:
+            user_input = user_input.lower()
+        if user_input not in valid_cases:
+            prompt('Invalid input, please retry')
+            continue
+
+        if user_input == '' and default is not None:
+            return cases[default][1]
+        return user_input
+
+
+def confirm(text: str, default: bool = None):
+    if default is not None:
+        default = 1 if default else 0
+    return select(text, cases=[('(Y)es', 'y',), ('(N)o', 'n')], default=default, case_sensitive=False) == 'y'
+
+
+
