00fc902a69c3a967c4f6d7b8b994446eb6f2b55f
greatyyx <bigyyx@gmail.com>
2019-02-15 11:08:11 -0800

    add new line when exiting

--- rltk/cli.py
+++ rltk/cli.py
@@ -73,6 +73,7 @@ class Progress(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self._end:
             prompt('\r' + self._end, new_line=False)
+        prompt('')  # new line
 
 
 progress = Progress
